244c0bf48318bedd27d514d37761135acd37feec
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-18 08:22:45 +0300

    Using Action Method Parameters

---
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("SimpleForm");
+        }
+
+        public ViewResult ReceiveForm()
+        {
+            var name = Request.Form["name"];
+            var city = Request.Form["city"];
+
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
